42c5651d363edec9e0fd6b3fb8fbfb0f11e733a0
Agus Marchi <agusmdev@gmail.com>
2023-04-22 18:47:38 -0300

    test

--- script.js
+++ script.js
@@ -1,13 +1,264 @@
-const CHAT_BUTTON_SIZE=50,CHAT_BUTTON_RADIUS=25,CHAT_BUTTON_BACKGROUND_COLOR="black",scriptTag=document.currentScript;let ICON_COLOR="white";const chatButton=document.createElement("div");chatButton.setAttribute("id","chatbase-bubble-button"),chatButton.style.position="fixed",chatButton.style.bottom="20px",chatButton.style.right="20px",chatButton.style.width="50px",chatButton.style.height="50px",chatButton.style.borderRadius="25px",chatButton.style.backgroundColor="black",chatButton.style.boxShadow="0 4px 8px 0 rgba(0, 0, 0, 0.2)",chatButton.style.cursor="pointer",chatButton.style.zIndex=999999999,chatButton.style.transition="all .2s ease-in-out",chatButton.addEventListener("mouseenter",t=>{chatButton.style.transform="scale(1.05)"}),chatButton.addEventListener("mouseleave",t=>{chatButton.style.transform="scale(1)"});const chatButtonIcon=document.createElement("div");chatButtonIcon.style.display="flex",chatButtonIcon.style.alignItems="center",chatButtonIcon.style.justifyContent="center",chatButtonIcon.style.width="100%",chatButtonIcon.style.height="100%",chatButtonIcon.style.zIndex=999999999,chatButton.appendChild(chatButtonIcon),chatButton.addEventListener("click",()=>{"none"===chat.style.display?(chat.style.display="flex",chatButtonIcon.innerHTML=getChatButtonCloseIcon()):(chat.style.display="none",chatButtonIcon.innerHTML=getChatButtonIcon())});const chat=document.createElement("div");chat.setAttribute("id","chatbase-bubble-window"),chat.style.position="fixed",chat.style.flexDirection="column",chat.style.justifyContent="space-between",chat.style.bottom="80px",chat.style.right="20px",chat.style.width="85vw",chat.style.height="70vh",chat.style.boxShadow="rgba(150, 150, 150, 0.15) 0px 6px 24px 0px, rgba(150, 150, 150, 0.15) 0px 0px 0px 1px",chat.style.display="none",chat.style.borderRadius="10px",chat.style.zIndex=999999999,chat.style.overflow="hidden",document.body.appendChild(chat),chat.innerHTML=`<iframe
-src="http://d6b3-181-4-97-194.ngrok.io"
+const CHAT_BUTTON_SIZE = 50,
+CHAT_BUTTON_RADIUS = 25,
+CHAT_BUTTON_BACKGROUND_COLOR = "black",
+scriptTag = document.currentScript;
+
+
+let ICON_COLOR = "white";
+
+function createElements() {
+
+const chatButton = document.createElement("div");
+chatButton.setAttribute("id", "interview-now-bubble-button");
+chatButton.style.position = "fixed";
+chatButton.style.bottom = "20px";
+chatButton.style.left = "20px";
+// chatButton.style.width = "50px";
+// chatButton.style.height = "50px";
+// chatButton.style.borderRadius = "25px";
+// chatButton.style.backgroundColor = "black";
+chatButton.style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2)";
+chatButton.style.cursor = "pointer";
+chatButton.style.zIndex = 999999999;
+chatButton.style.transition = "all .2s ease-in-out";
+
+chatButton.addEventListener("mouseenter", (t) => {
+  chatButton.style.transform = "scale(1.05)";
+});
+
+chatButton.addEventListener("mouseleave", (t) => {
+  chatButton.style.transform = "scale(1)";
+});
+
+const chatButtonIcon = document.createElement("div");
+chatButtonIcon.style.display = "flex";
+chatButtonIcon.style.alignItems = "center";
+chatButtonIcon.style.justifyContent = "center";
+chatButtonIcon.style.width = "100%";
+chatButtonIcon.style.height = "100%";
+chatButtonIcon.style.zIndex = 999999999;
+chatButton.appendChild(chatButtonIcon);
+
+chatButton.addEventListener("click", () => {
+  if (chat.style.display === "none") {
+    chat.style.display = "flex";
+    chatButtonIcon.innerHTML = getChatButtonCloseIcon();
+  } else {
+    chat.style.display = "none";
+    chatButtonIcon.innerHTML = getChatButtonIcon();
+  }
+});
+
+const chat = document.createElement("div");
+chat.setAttribute("id", "interview-now-bubble-window");
+chat.style.position = "fixed";
+chat.style.flexDirection = "column";
+chat.style.justifyContent = "space-between";
+chat.style.bottom = "80px";
+chat.style.left = "20px";
+chat.style.width = "85vw";
+chat.style.height = "60vh";
+// chat.style.backgroundColor = "black";
+// chat.style.boxShadow = "rgba(150, 150, 150, 0.15) 0px 6px 24px 0px, rgba(150, 150, 150, 0.15) 0px 0px 0px 1px";
+chat.style.display = "none";
+chat.style.borderRadius = "10px";
+chat.style.zIndex = 999999999;
+chat.style.overflow = "hidden";
+document.body.appendChild(chat);
+chat.innerHTML = `<iframe
+src="https://interviewnow.netlify.app"
 width="100%"
 height="100%"
 frameborder="0"
-></iframe>`;const mediaQuery=window.matchMedia("(min-width: 550px)");function handleChatWindowSizeChange(t){t.matches&&(chat.style.height="600px",chat.style.width="400px")}mediaQuery.addEventListener("change",handleChatWindowSizeChange),handleChatWindowSizeChange(mediaQuery);const getChatButtonColor=async()=>{chatButton.style.backgroundColor="black",document.body.appendChild(chatButton);let t=getContrastingTextColor("black");console.log("iconColor",t),ICON_COLOR=t,chatButtonIcon.innerHTML=getChatButtonIcon()};function getChatButtonIcon(){return`
-  <svg id="chatIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.3" stroke="${ICON_COLOR}" width="24" height="24">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-  </svg>`}function getChatButtonCloseIcon(){return`
-  <svg id="closeIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.3" stroke="${ICON_COLOR}" width="24" height="24">
-    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-  </svg>
-  `}function getContrastingTextColor(t){return"#"===t.charAt(0)&&(t=t.substr(1)),(.299*parseInt(t.substr(0,2),16)+.587*parseInt(t.substr(2,2),16)+.114*parseInt(t.substr(4,2),16))/255>.5?"black":"white"}getChatButtonColor();
+></iframe>`;
+
+const mediaQuery = window.matchMedia("(min-width: 550px)");
+
+function handleChatWindowSizeChange(t) {
+  if (t.matches) {
+    chat.style.height = "500px";
+    chat.style.width = "400px";
+  }
+}
+
+mediaQuery.addEventListener("change", handleChatWindowSizeChange);
+handleChatWindowSizeChange(mediaQuery);
+
+}
+
+//   const chatButton = document.createElement("div");
+//   chatButton.setAttribute("id", "interview-now-bubble-button");
+//   chatButton.style.position = "fixed";
+//   chatButton.style.bottom = "20px";
+//   chatButton.style.left = "20px";
+//   // chatButton.style.width = "50px";
+//   // chatButton.style.height = "50px";
+//   // chatButton.style.borderRadius = "25px";
+//   // chatButton.style.backgroundColor = "black";
+//   chatButton.style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2)";
+//   chatButton.style.cursor = "pointer";
+//   chatButton.style.zIndex = 999999999;
+//   chatButton.style.transition = "all .2s ease-in-out";
+
+//   chatButton.addEventListener("mouseenter", (t) => {
+//     chatButton.style.transform = "scale(1.05)";
+//   });
+
+//   chatButton.addEventListener("mouseleave", (t) => {
+//     chatButton.style.transform = "scale(1)";
+//   });
+
+//   const chatButtonIcon = document.createElement("div");
+//   chatButtonIcon.style.display = "flex";
+//   chatButtonIcon.style.alignItems = "center";
+//   chatButtonIcon.style.justifyContent = "center";
+//   chatButtonIcon.style.width = "100%";
+//   chatButtonIcon.style.height = "100%";
+//   chatButtonIcon.style.zIndex = 999999999;
+//   chatButton.appendChild(chatButtonIcon);
+
+//   chatButton.addEventListener("click", () => {
+//     if (chat.style.display === "none") {
+//       chat.style.display = "flex";
+//       chatButtonIcon.innerHTML = getChatButtonCloseIcon();
+//     } else {
+//       chat.style.display = "none";
+//       chatButtonIcon.innerHTML = getChatButtonIcon();
+//     }
+//   });
+
+//   const chat = document.createElement("div");
+//   chat.setAttribute("id", "interview-now-bubble-window");
+//   chat.style.position = "fixed";
+//   chat.style.flexDirection = "column";
+//   chat.style.justifyContent = "space-between";
+//   chat.style.bottom = "80px";
+//   chat.style.left = "20px";
+//   chat.style.width = "85vw";
+//   chat.style.height = "60vh";
+//   // chat.style.backgroundColor = "black";
+//   // chat.style.boxShadow = "rgba(150, 150, 150, 0.15) 0px 6px 24px 0px, rgba(150, 150, 150, 0.15) 0px 0px 0px 1px";
+//   chat.style.display = "none";
+//   chat.style.borderRadius = "10px";
+//   chat.style.zIndex = 999999999;
+//   chat.style.overflow = "hidden";
+//   document.body.appendChild(chat);
+//   chat.innerHTML = `<iframe
+// src="https://interviewnow.netlify.app"
+// width="100%"
+// height="100%"
+// frameborder="0"
+// ></iframe>`;
+
+//   const mediaQuery = window.matchMedia("(min-width: 550px)");
+
+//   function handleChatWindowSizeChange(t) {
+//     if (t.matches) {
+//       chat.style.height = "500px";
+//       chat.style.width = "400px";
+//     }
+//   }
+
+//   mediaQuery.addEventListener("change", handleChatWindowSizeChange);
+//   handleChatWindowSizeChange(mediaQuery);
+
+const getChatButtonColor = async () => {
+// chatButton.style.backgroundColor = "black";
+document.body.appendChild(chatButton);
+
+const textColor = getContrastingTextColor("black");
+ICON_COLOR = textColor;
+chatButtonIcon.innerHTML = getChatButtonIcon();
+};
+
+function addAnimationKeyframes() {
+const style = document.createElement('style');
+style.innerHTML = `
+@keyframes button-ping {
+  0% {
+    transform: scale(0.75);
+    box-shadow: 0 0 0 0 rgba(0,0,0, 0.5);
+  }
+  70% {
+    transform: scale(.95);
+    box-shadow: 0 0 0 10px rgba(0,0,0, 0);
+  }
+  100% {
+    transform: scale(0.75);
+    box-shadow: 0 0 0 0 rgba(0,0,0, 0);
+  }
+}
+`;
+document.head.appendChild(style);
+}
+
+function getChatButtonIcon() {
+return `
+<div style="position: relative; display: inline-block;" onmouseover="this.querySelector('button').style.backgroundColor='#1F2937'" onmouseout="this.querySelector('button').style.backgroundColor='#1F2937'">
+<button style="position: relative; z-index: 10; display: flex; align-items: center; justify-content: center; width: 9rem; height: 2.5rem; padding: 1rem 0.1rem 1rem;  font-weight: bold; color: #ffffff; background-color: #1F2937; border-radius: 0.5rem; outline: none; cursor: pointer; transition: background-color 0.3s; box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);">
+<span style="font-family: Arial, Helvetica, sans-serif;">Submit profile!</span>
+</button>
+<span style="position: absolute; top: 0; left: 0; display: flex; align-items: center; justify-content: center; width: 100%; height: 100%; padding: 1px; animation: button-ping 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite;">
+<span style="display: block; width: 9rem; height: 2.5rem; background-color: transparent; border: 1px solid rgba(75, 85, 99, 0.6); border-radius: 0.5rem;"></span>
+</span>
+</div>`;
+}
+function getChatButtonCloseIcon() {
+return `
+<div style="position: relative; display: inline-block;" onmouseover="this.querySelector('button').style.backgroundColor='#1F2937'" onmouseout="this.querySelector('button').style.backgroundColor='#1F2937'">
+<button style="position: relative; z-index: 10; display: flex; align-items: center; justify-content: center; width: 9rem; height: 2.5rem; padding: 1rem 0.1rem 1rem;  font-weight: bold; color: #ffffff; background-color: #1F2937; border-radius: 0.5rem; outline: none; cursor: pointer; transition: background-color 0.3s; box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);">
+<span style="font-family: Arial, Helvetica, sans-serif;">Close</span>
+</button>
+<span style="position: absolute; top: 0; left: 0; display: flex; align-items: center; justify-content: center; width: 100%; height: 100%; padding: 1px;">
+<span style="display: block; width: 9rem; height: 2.5rem; background-color: transparent; border: 1px solid rgba(75, 85, 99, 0.6); border-radius: 0.5rem;"></span>
+</span>
+</div>`;
+}
+
+function getContrastingTextColor(color) {
+if (color.charAt(0) === "#") {
+  color = color.substr(1);
+}
+return (
+  (0.299 * parseInt(color.substr(0, 2), 16) +
+    0.587 * parseInt(color.substr(2, 2), 16) +
+    0.114 * parseInt(color.substr(4, 2), 16)) /
+    255 >
+    0.5
+    ? "black"
+    : "white"
+);
+}
+
+function enabledPaths() {
+// Make a request to the endpoint to get the enabled paths
+const userId = scriptTag.getAttribute("userId")
+fetch(`https://n252jrjdnyzkxkxecnbmnhveme0sjlqi.lambda-url.us-east-1.on.aws/users/${userId}/paths`)
+  .then(response => response.json())
+  .then(data => {
+    // Extract the enabled paths from the response data
+    const enabledPaths = data.enabled_paths.map(e => e.toLowerCase());
+
+    // Get the current URL of the page
+    const currentUrl = window.location.href.toLowerCase();
+
+    // Check if the current URL is in the list of enabled domain paths
+    const bubbleButton = document.getElementById("interview-now-bubble-button")
+    const bubbleWindow = document.getElementById("interview-now-bubble-window")
+    if ((!bubbleButton && !bubbleWindow) && enabledPaths.includes(currentUrl) || enabledPaths.includes("*")) {
+      // Get the iframe element
+      createElements()
+    }
+    else {
+      bubbleButton.remove()
+      bubbleWindow.remove()
+
+    }
+  })
+  .catch(error => console.error("Error fetching enabled paths:", error));
+}
+
+addAnimationKeyframes()
+getChatButtonColor();
+enabledPaths()
+window.addEventListener("hashchange", enabledPaths);
