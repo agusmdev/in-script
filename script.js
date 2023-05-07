73f3793b41fce469b7b1482796f3804805546bab
Agus Marchi <agusmdev@gmail.com>
2023-05-07 10:48:51 -0300

    update shadow

--- script.js
+++ script.js
@@ -7,69 +7,63 @@ const userId = scriptTag.getAttribute("userId")
 
 let ICON_COLOR = "white";
 
-function createElements() {
+const closeButton = document.createElement("div");
+closeButton.setAttribute("id", "candidate-now-close-button");
+closeButton.style.position = "fixed";
+// closeButton.style.bottom = "20px";
+closeButton.style.bottom = "550px";
+closeButton.style.left = "20px";
+// closeButton.style.width = "50px";
+// closeButton.style.height = "50px";
+// closeButton.style.borderRadius = "25px";
+// closeButton.style.backgroundColor = "black";
+// closeButton.style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2)";
+closeButton.style.cursor = "pointer";
+closeButton.style.zIndex = 999999999;
+closeButton.style.transition = "all .2s ease-in-out";
+// closeButton.style.background = "red";
 
-const chatButton = document.createElement("div");
-chatButton.setAttribute("id", "interview-now-bubble-button");
-chatButton.style.position = "fixed";
-// chatButton.style.bottom = "20px";
-chatButton.style.bottom = "550px";
-chatButton.style.left = "20px";
-// chatButton.style.width = "50px";
-// chatButton.style.height = "50px";
-// chatButton.style.borderRadius = "25px";
-// chatButton.style.backgroundColor = "black";
-// chatButton.style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2)";
-chatButton.style.cursor = "pointer";
-chatButton.style.zIndex = 999999999;
-chatButton.style.transition = "all .2s ease-in-out";
-// chatButton.style.background = "red";
-
-chatButton.addEventListener("mouseenter", (t) => {
-  chatButton.style.transform = "scale(1.05)";
+closeButton.addEventListener("mouseenter", (t) => {
+closeButton.style.transform = "scale(1.05)";
 });
 
-chatButton.addEventListener("mouseleave", (t) => {
-  chatButton.style.transform = "scale(1)";
+closeButton.addEventListener("mouseleave", (t) => {
+closeButton.style.transform = "scale(1)";
 });
 
-const chatButtonIcon = document.createElement("div");
-chatButtonIcon.style.display = "flex";
-chatButtonIcon.style.alignItems = "center";
-chatButtonIcon.style.justifyContent = "center";
-chatButtonIcon.style.width = "100%";
-chatButtonIcon.style.height = "100%";
-chatButtonIcon.style.zIndex = 999999999;
-chatButton.appendChild(chatButtonIcon);
+const closeButtonIcon = document.createElement("div");
+// closeButtonIcon.style.display = "flex";
+closeButtonIcon.style.display = "none";
+closeButtonIcon.style.alignItems = "center";
+closeButtonIcon.style.justifyContent = "center";
+closeButtonIcon.style.width = "100%";
+closeButtonIcon.style.height = "100%";
+closeButtonIcon.style.zIndex = 999999999;
+closeButton.appendChild(closeButtonIcon);
 
-chatButton.addEventListener("click", () => {
-  if (chat.style.display === "none") {
-    chat.style.display = "flex";
-    chatButtonIcon.innerHTML = getChatButtonIcon();
-  } else {
-    chat.style.display = "none";
-    chatButtonIcon.innerHTML = ""
-    // chatButtonIcon.innerHTML = getChatButtonIcon();
-  }
+closeButton.addEventListener("click", () => {
+widgetForm.style.display = "none";
+closeButtonIcon.innerHTML = ""
 });
 
-const chat = document.createElement("div");
-chat.setAttribute("id", "interview-now-bubble-window");
-chat.style.position = "fixed";
-chat.style.bottom = "10px";
-chat.style.left = "20px";
-// chat.style.width = "85vw";
-// chat.style.height = "2000px";
-// chat.style.backgroundColor = "red";
-chat.style.boxShadow = "0 1px 14px 1px rgba(0,0,0,0.1)"
-chat.style.display = "flex";
-chat.style.borderRadius = "10px";
-chat.style.zIndex = 999999999;
-// chat.style.overflow = "visible";
-// chat.style.overflow = "hidden";
-document.body.appendChild(chat);
-chat.innerHTML = `<iframe
-src="https://interviewnow.netlify.app?userId=${userId}"
+const widgetForm = document.createElement("div");
+widgetForm.setAttribute("id", "candidate-now-widget");
+widgetForm.style.position = "fixed";
+widgetForm.style.bottom = "10px";
+widgetForm.style.left = "20px";
+// widgetForm.style.width = "85vw";
+// widgetForm.style.height = "2000px";
+// widgetForm.style.backgroundColor = "red";
+widgetForm.style.boxShadow = "0 15px 14px 1px rgba(0,0,0,0.1)"
+widgetForm.style.display = "none"
+// widgetForm.style.display = "flex";
+widgetForm.style.borderRadius = "10px";
+widgetForm.style.zIndex = 999999999;
+// widgetForm.style.overflow = "visible";
+// widgetForm.style.overflow = "hidden";
+document.body.appendChild(widgetForm);
+widgetForm.innerHTML = `<iframe
+src="https://8625-181-4-97-72.ngrok-free.app?userId=${userId}"
 width="100%"
 height="100%"
 frameborder="0"
@@ -79,28 +73,28 @@ frameborder="0"
 const mediaQuery = window.matchMedia("(min-width: 550px)");
 
 function handleChatWindowSizeChange(t) {
-  if (t.matches) {
-    chat.style.height = "550px";
-    // chat.style.height = "500px";
-    chat.style.width = "400px";
-  }
+if (t.matches) {
+  widgetForm.style.height = "550px";
+  // widgetForm.style.height = "500px";
+  widgetForm.style.width = "400px";
+}
 }
 
 mediaQuery.addEventListener("change", handleChatWindowSizeChange);
 handleChatWindowSizeChange(mediaQuery);
 
-const getChatButtonColor = async () => {
-  // chatButton.style.backgroundColor = "black";
-  document.body.appendChild(chatButton);
+const getCloseButtonColor = async () => {
+// closeButton.style.backgroundColor = "black";
+document.body.appendChild(closeButton);
 
-  const textColor = getContrastingTextColor("black");
-  ICON_COLOR = textColor;
-  chatButtonIcon.innerHTML = getChatButtonIcon();
+const textColor = getContrastingTextColor("black");
+ICON_COLOR = textColor;
+closeButtonIcon.innerHTML = getCloseButtonIcon();
 };
 
 function addAnimationKeyframes() {
-  const style = document.createElement('style');
-  style.innerHTML = `
+const style = document.createElement('style');
+style.innerHTML = `
 @keyframes button-ping {
 0% {
 transform: scale(0.75);
@@ -116,36 +110,35 @@ box-shadow: 0 0 0 0 rgba(0,0,0, 0);
 }
 }
 `;
-  document.head.appendChild(style);
+document.head.appendChild(style);
 }
 
-function getChatButtonIcon() {
-  return `
+function getCloseButtonIcon() {
+return `
 <div style="position: relative; display: inline-block;" >
 <div style="position: relative; z-index: 10; display: flex; align-items: center; justify-content: center; width: fit-content; height: 0.5rem; padding: 1rem 1.5rem; margin: 0 30px; font-weight: 200; color: #0f0f0f; background-color: #fff; border-radius: 0.5rem 0.5rem 0 0 ; outline: none; cursor: pointer; ">
 <span style="font-family: Arial, Helvetica, sans-serif;">Close</span>
 </div>
 </div>`;
 }
+
 function getContrastingTextColor(color) {
-  if (color.charAt(0) === "#") {
-    color = color.substr(1);
-  }
-  return (
-    (0.299 * parseInt(color.substr(0, 2), 16) +
-      0.587 * parseInt(color.substr(2, 2), 16) +
-      0.114 * parseInt(color.substr(4, 2), 16)) /
-      255 >
-      0.5
-      ? "black"
-      : "white"
-  );
+if (color.charAt(0) === "#") {
+  color = color.substr(1);
+}
+return (
+  (0.299 * parseInt(color.substr(0, 2), 16) +
+    0.587 * parseInt(color.substr(2, 2), 16) +
+    0.114 * parseInt(color.substr(4, 2), 16)) /
+    255 >
+    0.5
+    ? "black"
+    : "white"
+);
 }
 
-getChatButtonColor()
+getCloseButtonColor()
 addAnimationKeyframes()
-
-}
 
 function enabledPaths() {
 // Make a request to the endpoint to get the enabled paths
@@ -159,15 +152,19 @@ fetch(`https://n252jrjdnyzkxkxecnbmnhveme0sjlqi.lambda-url.us-east-1.on.aws/user
     const currentUrl = window.location.href.toLowerCase();
 
     // Check if the current URL is in the list of enabled domain paths
-    const bubbleButton = document.getElementById("interview-now-bubble-button")
-    const bubbleWindow = document.getElementById("interview-now-bubble-window")
-    if (paths.some(url => url.startsWith(currentUrl.slice(0, -1))) || paths.includes("*")) {
+    // const bubbleButton = document.getElementById("candidate-now-close-button")
+    // const bubbleWindow = document.getElementById("candidate-now-widget")
+    const enabledSize = window.screen.width > 700
+
+    if (enabledSize && (paths.some(url => url.startsWith(currentUrl.slice(0, -1))) || paths.includes("*"))) {
       // Get the iframe element
-      createElements()
+      widgetForm.style.display = "flex";
+      closeButtonIcon.style.display = "flex";
+
     }
     else {
-      bubbleButton.remove()
-      bubbleWindow.remove()
+      widgetForm.style.display = "none";
+      closeButtonIcon.style.display = "none";
 
     }
   })
